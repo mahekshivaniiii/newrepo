64ac3087d41fe898c5c652a29dd8099688bbdd77
mahekshivaniiii <mahekshivani42@gmail.com>
2025-01-08 00:13:12 +0530

    typecasting

---
+++ typecasting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // implict type casting or conversion which is done by compiler automatically char --> int 
+    char grade = 'A';
+
+    int value = grade ;
+    cout << value << endl;
+
+    //explict type casting which is done by programmers int --> double
+    double value1 = 100.01 ;
+    double value2 = 100.9909 ;
+    int newvalue1 = (int)value1;
+    int newvalue2 = (int)value2;
+    cout << newvalue1 << endl << newvalue2 << endl; 
+
+
+    
+	return 0;
+}
