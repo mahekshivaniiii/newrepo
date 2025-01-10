416c30cbd2c165f2325811febfd54a86e1a6c99f
mahekshivaniiii <mahekshivani42@gmail.com>
2025-01-11 00:19:57 +0530

    optimizing using root in prime number

---
+++ dowhileLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    do{
+        cout << "Hello World" << endl;
+
+    }while(3 > 5);
+    return 0;
+}
---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // 5 7 11
+   int count = 0;
+   int n ;
+   cin >> n;
+   for(int i = 2; i*i<=n ; i ++){
+    if(n%i==0){
+        count ++;
+    }
+   }
+   if(count == 2){
+    cout << "Prime" << endl;
+   } 
+   else{
+    cout << "Non-Prime" << endl;
+   }
+    return 0;
+}
